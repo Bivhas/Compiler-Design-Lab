fc603c1a37b5b1133a25a57a443d6ed821aa223b
Bivhas Devkota <167157922+Bivhas@users.noreply.github.com>
2026-05-07 16:55:34 +0545

    Lab 1 Compiler Design

---
+++ Lab1/lab1.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main() { 
+int a, b, sum; 
+printf("Enter two numbers: "); // It is for printing
+scanf("%d %d", &a, &b); 
+sum = a + b; 
+printf("Sum = %d\n", sum); 
+return 0; 
+} 
